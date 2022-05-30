adf142ebe97ef9bab099daf5867e72c84351d609
spatial-clash <lemonlefty@outlook.com>
2022-05-30 12:22:08 -0600

    Hello World App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import earth from './earth.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={earth} className="App-logo" alt="Hello" />
+        <p>
+         Hello, World!
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
